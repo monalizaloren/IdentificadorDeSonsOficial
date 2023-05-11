315acde9282a7838cbf3239b45e8bac4e3b3f58c
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-05-11 16:23:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 
+
+    img = document.getElementById('alien1') 
+    ??? = document.getElementById('???')
+    ??? = document.getElementById('???')
+    ??? = document.getElementById('???')
+
+    if (results[0].label == "Palmas") {
+      img.src = 'aliens-01.gif';
+      img1.src = 'aliens-02.png';
+      img2.src = 'aliens-03.png';
+      img3.src = 'aliens-04.png';
+    } else if (results[0].label == "Sino") {
+      img.src = 'aliens-01.png';
+      img1.src = 'aliens-02.gif';
+      img2.src = 'aliens-03.png';
+      img3.src = 'aliens-04.png';
+    } else if (results[0].label == "Estalos") {
+      img.src = 'aliens-01.png';
+      img1.src = 'aliens-02.png';
+      img2.src = 'aliens-03.gif';
+      img3.src = 'aliens-04.png';
+    }else{
+      img.src = 'aliens-01.png';
+      img1.src = 'aliens-02.png';
+      img2.src = 'aliens-03.png';
+      img3.src = 'aliens-04.gif';
+    }
+  }
+}
